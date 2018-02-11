edef8cd24e1a860bc1b5a58caeab7f76efb4fb3a
Ariel De Ocampo <arielmakestuff@gmail.com>
2018-02-11 07:41:39 -0500

    Copy CodeConvert from original core module

--- src/core/new.rs
+++ src/core/new.rs
@@ -14,7 +14,55 @@ use chrono::{DateTime, Utc};
 // use failure::Fail;
 
 // Local imports
-use core::{CodeConvert, CodeValueError};
+
+// ===========================================================================
+// CodeConvert
+// ===========================================================================
+
+#[derive(Fail, Debug)]
+#[fail(display = "Unknown code value: {}", code)]
+pub struct CodeValueError
+{
+    pub code: u64,
+}
+
+/// Allows converting between a number and a type.
+///
+/// The type implementing [`CodeConvert`] will usually be an enum that defines
+/// different codes.
+///
+/// # Assumptions
+///
+/// This trait assumes the following of the implementing enum:
+///
+/// 1. The enum is a C-style enum
+/// 2. The enum's values are unsigned integers
+/// 3. The enum's values are continuous without any gaps ie 0, 1, 2 are valid
+///    values but 0, 2, 4 is not
+///
+/// [`CodeConvert`]: trait.CodeConvert.html
+pub trait CodeConvert<T>: Clone + PartialEq
+{
+    type int_type;
+
+    /// Convert a number to type T.
+    fn from_number(num: Self::int_type) -> Result<T, CodeValueError>;
+
+    /// Convert a u64 to type T.
+    fn from_u64(num: u64) -> Result<T, CodeValueError>;
+
+    /// Convert type T to a number.
+    fn to_number(&self) -> Self::int_type;
+
+    /// Convert type T to a u64.
+    fn to_u64(&self) -> u64;
+
+    /// Return the maximum number value
+    fn max_number() -> u64;
+
+    /// Cast a u64 number into acceptable int type
+    fn cast_number(n: u64) -> Option<Self::int_type>;
+}
 
 // ===========================================================================
 // General message
